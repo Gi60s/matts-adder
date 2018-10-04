410adc78eba84e1b6d179ed074526b162bc2de49
james <james.speirs@gmail.com>
2018-10-04 17:23:08 -0600

    can add any number of numbers

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function adder(string) {
     if (!string) return 0
 
     const ar = string.split(',')
-    if (ar.length > 2) throw Error('Too many numbers')
 
     let result = 0
     ar.forEach(v => {
--- test/index.test.js
+++ test/index.test.js
@@ -16,8 +16,4 @@ describe('adder', () => {
         expect(adder('1,2')).to.equal(3)
     })
 
-    it('throws an error when 3 numbers provided', () => {
-        expect(() => adder('1,2,3')).to.throw(Error)
-    })
-
 })

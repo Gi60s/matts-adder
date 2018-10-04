ec51cbf17beffa86c5e26dce823acfc25302ac25
james <james.speirs@gmail.com>
2018-10-04 17:14:00 -0600

    can also parse 1 or 2 numbers

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,15 @@ const expect = require('chai').expect
 describe('adder', () => {
 
     it('can parse 0 numbers', () => {
-        expect(adder.parse('')).to.equal(0)
+        expect(adder('')).to.equal(0)
+    })
+
+    it('can parse one number', () => {
+        expect(adder('1')).to.equal(1)
+    })
+
+    it('can parse two numbers', () => {
+        expect(adder('1,2')).to.equal(1)
     })
 
 })

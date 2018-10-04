61a2a777ba1f00f9b775884324bb2dcdba428561
james <james.speirs@gmail.com>
2018-10-04 17:43:15 -0600

    add regexp delimiter parser

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 'use strict'
 
+const rxInit = /^(?:\/\/(.+)$)?([\s\S]*)/m
+
 module.exports = adder
 
 function adder(string) {
     if (!string) return 0
 
-    const ar = string.split(/,|\n/)
+    const match = rxInit.exec(string)
+    const delimiter = match[1] || ','
+    const numbersString = match[2] || ''
+
+    const rx = RegExp(escapeRegExp(delimiter) + '|\n');
+    const numbers = numbersString.split(rx)
 
     let result = 0
-    ar.forEach(v => {
+    numbers.forEach(v => {
         result += +v
     })
 
     return result
+}
+
+function escapeRegExp(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }

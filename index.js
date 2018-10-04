04eba2c1982319c8088203ec5ee2f8d929052cd2
james <james.speirs@gmail.com>
2018-10-04 17:48:39 -0600

    throw error for negative numbers

--- index.js
+++ index.js
@@ -6,22 +6,32 @@ module.exports = adder
 
 function adder(string) {
     if (!string) return 0
+    const negativeNumbers = []
 
     const match = rxInit.exec(string)
     const delimiter = match[1] || ','
     const numbersString = match[2] || ''
 
-    const rx = RegExp(escapeRegExp(delimiter) + '|\n');
+    const rx = RegExp(escapeRegExp(delimiter) + '|\n')
     const numbers = numbersString.split(rx)
 
     let result = 0
     numbers.forEach(v => {
-        result += +v
+        const num = +v
+        if (num < 0) {
+          negativeNumbers.push(num)
+        } else {
+            result += num
+        }
     })
+
+    let errMessage = ''
+    if (negativeNumbers.length) errMessage += 'Negative numbers not allowed: ' + negativeNumbers.join(', ')
+    if (errMessage) throw Error(errMessage)
 
     return result
 }
 
 function escapeRegExp(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
 }

046c5515480adf3383b08e1fe7b9ca7d6ab4dd24
james <james.speirs@gmail.com>
2018-10-04 17:21:00 -0600

    can parse any number of comma separated values

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ module.exports = adder
 
 function adder(string) {
     if (!string) return 0
+
+    const ar = string.split(',')
+    if (ar.length > 2) throw Error('Too many numbers')
+
+    let result = 0
+    ar.forEach(v => {
+        result += +v
+    })
+
+    return result
 }
